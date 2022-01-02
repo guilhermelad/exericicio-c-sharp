5a1a88399f8e23e1bae9add01314fe5912d26e2e
Guilherme Ladeira <gui.o.ladeira@gmail.com>
2022-01-02 17:42:17 -0300

    Novo Exercicio

---
+++ Poo/ExericicoClasseEstaticaDolar/Conversor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExericicoClasseEstaticaDolar
+{
+    internal class Conversor
+    {
+
+        public static double IOF = 0.06;
+
+        public static double RealDolar(double cotacao, double valor)
+        {
+            
+            return (cotacao * valor) + (cotacao * valor) * IOF;
+        }
+
+
+
+
+    }
+}
---
+++ Poo/ExericicoClasseEstaticaDolar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExericicoClasseEstaticaDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dólar? ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = Conversor.RealDolar(cot, val);
+            Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
