b6c67311d3867c9e654361d54921186824ffb30f
Guilherme Ladeira <gui.o.ladeira@gmail.com>
2022-01-03 00:04:43 -0300

    Novo Exercício
    Nesse exercício há a utilização de encapsulamento, properties, this, construtores e etc

---
+++ Poo/ExercicioPooBanco/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioPooBanco
+{
+     class ContaBancaria
+    {
+        public string Titular { get; set; }
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(string nome, int numero)
+        {
+            Titular = nome;
+            Numero = numero;
+            
+        }
+
+        public ContaBancaria(int numero, string nome, double saldo) : this(nome, numero)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo = Saldo - valor - 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: R$"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+        
+
+
+
+
+    }
+}
---
+++ Poo/ExercicioPooBanco/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioPooBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valordeposito, valorsaque;
+            
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o Titular da conta: ");
+            string nometitular = Console.ReadLine();
+            Console.Write("Haver deposito inicial? (s/n)");
+            char validar = char.Parse(Console.ReadLine());
+            ContaBancaria conta = new ContaBancaria(nometitular, numeroconta);
+            if (validar == 's')
+            {
+                Console.Write("Entre com o valor de depósito incial: ");
+                valordeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Deposito(valordeposito);
+                
+            }
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor de depósito: ");
+            valordeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valordeposito);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor de saque: ");
+            valorsaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valorsaque);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+
+
+        }
+    }
+}
