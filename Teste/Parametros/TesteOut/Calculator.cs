1755a79f6fe0ccca27fb781834a55992f4c90d13
Guilherme Ladeira <gui.o.ladeira@gmail.com>
2022-01-10 14:32:30 -0300

    Teste de out

---
+++ Teste/Parametros/TesteOut/Calculator.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace TesteOut
+{
+    class Calculator
+    {
+
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Teste/Parametros/TesteOut/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TesteOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+
+        }
+    }
+}
