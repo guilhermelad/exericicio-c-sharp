2c1b28a81006d84b582ce7e60ee736f6bdead798
Guilherme Ladeira <gui.o.ladeira@gmail.com>
2022-01-10 14:23:20 -0300

    Nova pasta para teste de código

---
+++ Teste/Parametros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TesteModiParametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(1, 2);
+            int s2 = Calculator.Sum(10, 10, 10, 10);
+
+
+            Console.WriteLine(s1); 
+            Console.WriteLine(s2);  
+        }
+    }
+}
