749d6f1d5eb45117ddac3ed28b54778e2fac0a49
Guilherme Ladeira <gui.o.ladeira@gmail.com>
2022-01-16 18:41:34 -0300

    Lista
    Novo teste de lista e exercício de lista

---
+++ Lista/ExercicioListaSalario/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioListaSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> lista = new List<Employee>();
+
+            Console.WriteLine("How many employees will be registered: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employ #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                lista.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id tha will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = lista.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This ID does not exist");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Update list of employees: ");
+            foreach (Employee obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
---
+++ Teste/Lista/TesteLista/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TesteLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("Número de informações da lista: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("Primeiro nome com letra A: " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Último nome com a letra A: " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Primeira posição A: " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Ultima posição A: " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("============================");
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.Remove("Alex");
+
+            Console.WriteLine("============================");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+
+
+            Console.WriteLine("============================");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAt(1); 
+            Console.WriteLine("============================");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+
+    }
+}
